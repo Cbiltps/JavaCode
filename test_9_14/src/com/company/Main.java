a95723babda0437c280dfa037b72cee7fe13dc7e
Cbiltps <1239669254@qq.com>
2021-09-14 20:59:19 +0800

    这个是今天的学习日志！

---
+++ test_9_14/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// 这个是今天的学习日志，今天讲了二维数组，开始最重要的面向对象
+        //面向对象的三大特征：
+        //1:封装  为了安全
+        //2:继承  复用代码
+        //3:多态
+
+        //类是对象的抽象，用于描述一组对象的共同特征和行为
+
+        //对象的创建与使用：
+        //java中用new关键字创建对象
+        //类名 对象名称 = new 类名（ ） 举例：Person p = new Person （ ）
+        //这样变量p便持有了对象的引用 其实可以看做C语言的指针
+    }
+}
