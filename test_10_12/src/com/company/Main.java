d3e2ecaf0a6e9bd33e968982002fc9ffb78e94db
Cbiltps <1239669254@qq.com>
2021-10-14 00:54:10 +0800

    第二次作业的测试代码！

---
+++ test_10_12/src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+//这个是博哥Java的作业！
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        short a =128;
+//        byte b =(byte) a;
+//        System.out.println(a);// 128
+//        System.out.println(b);// -128 为什么？
+        //这个是作业。到时候好好听课！
+
+//        double x =2.0;
+//        int y = 4;
+//        x /= ++y;
+//        System.out.println(x);
+
+        //int  a = 3;
+        //byte b = a;// 不可以这样写！
+
+        //byte a = 130;//超出范围
+
+        //System.out.print("%d\n");
+        System.out.println("\\\"hello\\\"");
+        //System.out.println("\\"hello\\"");
+        System.out.println("/\"hello/\"");
+
+//        switch (true) {
+//
+//        }
+    }
+}
