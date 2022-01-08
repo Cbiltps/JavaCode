382c62bded456cbbabde98277a8a0a65d28b180f
Cbiltps <1239669254@qq.com>
2022-01-08 18:00:10 +0800

    这是空白代码（测试子类构造）

---
+++ test_12_2/src/com/xiangzi/www/TestDemo.java
@@ -0,0 +1,27 @@
+package com.xiangzi.www;
+
+class Animal {
+    public String name;
+    public int age;
+
+    public Animal(String name,int age) { //父类的构造方法
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class Dog extends Animal {
+    public Dog (String name,int age){ //关于子类的构造方法，看知识点的截图
+        super(name, age);
+    }
+}
+
+public class TestDemo {
+    public static void main(String[] args) {
+//        Animal animal = new Dog("哈士奇",19);//父类引用 引用 子类对象
+//        System.out.println(animal.age);
+        Dog dog = new Dog("金毛",20);
+        System.out.println(dog.age);
+        System.out.println("123");
+    }
+}
