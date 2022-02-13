e5538b2f4e7190e861d9b5ce2b05e8705a8e89fa
Cbiltps <1239669254@qq.com>
2022-02-13 23:29:29 +0800

    添加异常的学习

--- test_2_10/src/com/company/Main.java
+++ test_2_10/src/com/company/Main.java
@@ -1,8 +1,37 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+    // 发生异常后面的语句不会执行
     public static void main(String[] args) {
+        int[] array = {1,2,3};
+        System.out.println(array[5]);
+        System.out.println("其他的功能");
+    }
+
+    // 这也发生一种异常
+    public static void main3(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(n);
+    }
+
+    public static void main2(String[] args) {
+        int[] array = {1,2,3};
+        try {
+            array = null;
+            System.out.println(array[2]);
+            System.out.println("haha!");
+        }catch (ArrayIndexOutOfBoundsException | NullPointerException e) { // 连续捕捉异常中间加“|”符号
+            e.printStackTrace();
+            System.out.println("我捕捉到了一个数组越界/空指针异常，这里开始处理异常了");
+        }
+        System.out.println("哈喽！！！");
+    }
+
+    public static void main1(String[] args) {
         int[] array = {1,2,3};
         try {
             System.out.println(array[9]);
