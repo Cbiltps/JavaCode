9062acadea6f1dd443e99b4dcca6491e9d9576a8
Cbiltps <1239669254@qq.com>
2022-04-07 10:03:31 +0800

    多线程编程 状态和线程安全

--- test_4_5/src/TestDemo2.java
+++ test_4_5/src/TestDemo2.java
@@ -1,4 +1,37 @@
+class Counter {
+    // 这个 变量 就是两个线程要去自增的变量
+    volatile public int count;
+
+    // java中解决线程安全的典型手段就是 加锁
+    synchronized public void increase() { // 这里加锁之后就是1_0000，不加就是5000和1_之间
+        count++;
+    }
+}
 
 public class TestDemo2 {
+    private static Counter counter = new Counter();
 
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 50000; i++) {
+                counter.increase();
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 50000; i++) {
+                counter.increase();
+            }
+        });
+        t1.start();
+        t2.start();
+
+        // 必须要在 t1 和 t2 都执行完了之后, 在打印 count 的结果.
+        // 否则, main 和 t1 t2 之间都是并发的关系~~, 导致 t1 和 t2 还没执行完, 就先执行了下面的 打印 操作
+        t1.join();
+        t2.join();
+
+        // 在 main 中打印一下两个线程自增完成之后, 得到的 count 结果~~
+        System.out.println(counter.count);
+    }
 }
+
