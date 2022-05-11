32bfd5bbc5fc7aa47aa7a35236294d33eb70f5d9
Cbiltps <1239669254@qq.com>
2022-05-11 23:41:31 +0800

    网络编程

---
+++ test_5_6/src/network/UdpEchoServer.java
@@ -0,0 +1,51 @@
+package network;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+/**
+ * 写一个最简单的客户端服务器程序，回显服务~ EchoServer
+ */
+public class UdpEchoServer {
+    // 进行网络编程, 第一步就需要先准备好 socket 实例~ 这是进行网络编程的大前提.
+    private DatagramSocket socket = null;
+
+    public UdpEchoServer(int port) throws SocketException {
+        socket = new DatagramSocket(port);
+    }
+
+    // 启动服务器
+    public void start() throws IOException {
+        System.out.println("启动服务器！");
+        // UDP不需要建立链接，直接接收从客服端发来的数据即可
+        while (true) {
+            // 1. 读取客户端发来的请求
+            DatagramPacket requestSocket = new DatagramPacket(new byte[1024], 1024);
+            socket.receive(requestSocket);// 为了接受数据, 需要先准备好一个空的 DatagramPacket 对象, 由 receive 来进行填充数据
+
+            // 把 DatagramPacket 解析成一个 String
+            String request = new String(requestSocket.getData(), 0, requestSocket.getLength(), "UTF-8");
+
+            // 2. 根据请求计算响应（由于这是一个回显服务，2省略)
+            String response = process(request);
+
+            // 3. 把响应写回客户端
+
+        }
+    }
+
+    /**
+     * 由于是回显服务, 响应就和请求一样了.
+     * 实际上对于一个真实的服务器来说, 这个过程是最复杂的. 为了实现这个过程, 可能需要几万行, 几十万行代码....
+     * @param request
+     * @return
+     */
+    private String process(String request) {
+        return  request;
+    }
+
+
+}
