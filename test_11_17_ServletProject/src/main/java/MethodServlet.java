d3b486da53804e4d6ee9866c443c79700b92ce9d
Cbiltps <1239669254@qq.com>
2022-11-18 19:22:28 +0800

    继续学习servlet

---
+++ test_11_17_ServletProject/src/main/java/MethodServlet.java
@@ -0,0 +1,15 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/method")
+public class MethodServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=utf8");
+        resp.getWriter().write("post 响应");
+    }
+}
